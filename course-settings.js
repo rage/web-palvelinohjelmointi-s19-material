5b60512a46798554c8114c9da031ab81e4b61661
Jami Kousa <jami.kousa@helsinki.fi>
2019-10-24 12:38:52 +0300

    projekti information as part of course material

--- course-settings.js
+++ course-settings.js
@@ -22,6 +22,7 @@ const courseSettings = {
       path: "/arvostelu-ja-kokeet",
     },
     { title: "Tukiväylät", path: "/tukivaylat" },
+    // { title: "Projekti", path: "/projekti"},
     {
       title: "Usein kysytyt kysymykset",
       path: "/usein-kysytyt-kysymykset",
